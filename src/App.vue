46ec9bdb12711c33aa4a510954cc00ee13da31e8
Marcelo Brasil <marcelo@evostack.com.br>
2024-04-07 22:55:18 -0300

    first commit / curva s concluída

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,78 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="legenda-container">
+    <p class="legenda">
+      <span class="cor-legenda projetada"></span> % do budget gasto (projeção)
+    </p>
+    <p class="legenda">
+      <span class="cor-legenda real"></span> % do budget gasto (real)
+    </p>
+  </div>
+  <div class="grafico-container">
+    <Line :data="chartData.data_projetada" class="grafico" />
+    <Line :data="chartData.data_real" class="grafico" />
+  </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+<script setup lang="ts">
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Line } from "vue-chartjs";
+import * as chartConfig from "@/data/curvaSData";
+import { ref } from "vue";
 
-export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-});
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title
+  // Tooltip
+);
+
+const chartData = ref(chartConfig);
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+.grafico-container {
+  position: relative;
+}
+
+.grafico {
+  position: absolute;
+}
+
+.legenda-container {
+  display: flex;
+  justify-content: center;
+  gap: 2rem;
+}
+
+.legenda {
+  font-family: Verdana, Geneva, Tahoma, sans-serif;
+  display: flex;
+  align-items: center;
+  gap: 0.25rem;
+}
+
+.cor-legenda {
+  width: 10px;
+  height: 10px;
+  border-radius: 50%;
+}
+
+.projetada {
+  background-color: #ccc;
+}
+
+.real {
+  background-color: #000;
 }
 </style>
---
+++ src/data/curvaSData.ts
@@ -0,0 +1,24 @@
+export const data_projetada = {
+    labels: ['Semana 1', 'Semana 2', 'Semana 3', 'Semana 4', 'Semana 5', 'Semana 6', 'Semana 7', 'Semana 8', 'Semana 9', 'Semana 10', 'Semana 11'],
+    datasets: [
+        {
+            backgroundColor: '#000',
+            data: [0, 3, 8, 15, 24, 45, 70, 85, 93, 98, 100]
+        }
+    ]
+}
+
+export const data_real = {
+    labels: ['Semana 1', 'Semana 2', 'Semana 3', 'Semana 4', 'Semana 5', 'Semana 6', 'Semana 7', 'Semana 8', 'Semana 9', 'Semana 10', 'Semana 11'],
+    datasets: [
+        {
+            backgroundColor: '#CCC',
+            data: [0, 4, 7, 17, 28, 40, 74, 87, 92, 97, 100]
+        }
+    ]
+}
+
+export const options = {
+    responsive: true,
+    maintainAspectRatio: false
+}
